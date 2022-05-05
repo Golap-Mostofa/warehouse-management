d0897746462829d1d4920cd1b6f85786896a8eee
Golap-mostofa <mostofa6410@gmail.com>
2022-05-06 01:53:22 +0600

    implement product cloction

--- src/components/AddProduct/AddProduct.js
+++ src/components/AddProduct/AddProduct.js
@@ -2,7 +2,23 @@ import React from 'react';
 import { useForm } from "react-hook-form";
 const AddProduct = () => {
     const { register, handleSubmit } = useForm();
-    const onSubmit = data => console.log(data);
+
+    const onSubmit = data => {
+        console.log(data)
+        const url = `http://localhost:4000/product`
+        fetch(url,{
+            method: 'POST',
+            headers:{
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+        .then(res=>res.json())
+        .then(result =>{
+            console.log(result);
+        })
+
+    };
 
 
     return (
@@ -11,10 +27,12 @@ const AddProduct = () => {
             <form className='w-80 mt-8 mx-auto flex flex-col' onSubmit={handleSubmit(onSubmit)}>
                 <p className='text-lime-700'>name</p>
                 <input className='bg-neutral-400 mb-3 py-2 px-4 rounded' placeholder='Name' {...register("Name", { required: true, maxLength: 20 })} />
+                <p className='text-lime-700'>photo url</p>
+                <input className='bg-neutral-400 mb-3 py-2 px-4 rounded' placeholder='Name' {...register("img", )} />
                 <p className='text-lime-700'>queantity</p>
                 <input className='bg-neutral-400 mb-3 py-2 px-4 rounded' placeholder='Discription' {...register("gueantity",)} />
-                <p className='text-lime-700'>Price</p>
-                <input className='bg-neutral-400 mb-3 py-2 px-4 rounded' placeholder='Price' type="number" {...register("price",)} />
+                <p className='text-lime-700'>discription</p>
+                <textarea className='bg-neutral-400 mb-3 py-2 px-4 rounded' placeholder='Price' type="number" {...register("discription",)} />
                 <input className='uppercase bg-lime-600 py-2 px-4 font-semibold text-white rounded mt-4' type="submit" />
             </form>
         </div>
